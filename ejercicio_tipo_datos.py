a415f269e030b998f04994ec17037283603c8b8c
Gianmarco <gianmarco.contreras@colossusperu.com>
2020-09-30 23:12:33 -0500

    Terminamos ejercicio tipo_datos y condicionales

---
+++ ejercicio_tipo_datos.py
@@ -0,0 +1,13 @@
+#PANADERÍA HARINITA
+
+pan_fresco = 3.49
+pan_pasado = pan_fresco - pan_fresco * 0.6
+
+pan_pasado_vendido = input("¿ Cuantos panes pasados se vendieron ? ")
+print("El precio habitual del pan es: " + str(pan_fresco))
+print("El descuento del pan pasado es del 60%: " + str(pan_fresco * 0.6))
+print("El precio del pan pasado es: " + str(pan_pasado))
+
+print("Precio pan fresco en soles: " + str(pan_fresco * 4.22))
+print("Precio pan pasado en soles: " + str(pan_pasado * 4.22))
+# ------------------------- OK --------------------------------------------------------
