9f20b53e770b138642738b4254491f58ac6a295f
Gianmarco <gianmarco.contreras@colossusperu.com>
2020-12-09 12:03:51 -0500

    Arreglos necesarios

--- Algoritmo_Horarios3/horarios3.py
+++ Algoritmo_Horarios3/horarios3.py
@@ -1,75 +1,76 @@
 class Main:
 
     def __init__(self):
-        self.__Dxcolumna = 0
-        self.__Nxcolumna = 0
-        self.__descansos_fila = 0
+        self.semana = 7
+        self.descansos_trabajador_semana = 0
+        self.cantidad_unidades = 3
+        self.turno_dia = 3
+        self.turno_noche = 5
 
-        self.__oficina = [['D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X'],
-                          ['N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N']]
+                        # 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27   28   29   30
+        self.oficina = [['D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X'],
+                        ['N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N']]
 
-        self.__almacen = [['D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D'],
-                          ['N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N'],
-                          ['N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N']]
+        self.almacen = [['D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D'],
+                        ['N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N'],
+                        ['N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N']]
 
-        self.__peaje =   [['D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D'],
-                          ['N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N'],
-                          ['N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N']]
+        self.peaje =   [['D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D'],
+                        ['N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N'],
+                        ['N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'N', 'N', 'N']]
 
-        self.__descanse= [['X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D']]
+        self.descansero=[['X','D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D', 'D', 'D', 'D', 'D', 'D', 'X', 'N', 'N', 'N', 'N', 'N', 'N', 'X', 'D']]
 
-        self.__reten =   [['X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X']]
+        self.reten =   [['X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X', 'X', 'N', 'X', 'N', 'X', 'X', 'X']]
 
-        self.__rol_servicio = [
-            {'U1': self.__oficina},
-            {'U2': self.__almacen},
-            {'U3': self.__peaje},
-            {'U4': self.__descanse},
-            {'U5': self.__reten}
+        self.rol_servicio = [
+            {'U1': self.oficina},
+            {'U2': self.almacen},
+            {'U3': self.peaje},
+            {'descansero': self.descansero},
+            {'reten': self.reten}
         ]
 
-        self.__unidad_validacion = \
+        self.unidad_validacion = \
             {'U1': [1, 1],  # diurno - nocturno
              'U2': [1, 2],
              'U3': [1, 2],
-             'U4': [1, 0],
-             'U5': [0, 1]}
+             'descansero': [1, 0],
+             'reten': [0, 1]}
 
-    # Validamos que cada trabajador tenga 4 o 5 descansos, excepto el reten - Validación por fila
+
+
+    # Validamos que cada trabajador tenga 1 descanso por semana (excepto el reten) - Validación por fila
     def validacion_descanso_fila(self):
-        for unidad in self.__rol_servicio:
+        for unidad in self.rol_servicio:
             for key, value in unidad.items():
                 for j in range(len(unidad[key])):
-                    self.__descansos_fila = 0
+                    self.descansos_trabajador_semana = 0
                     for i in range(len(value[0])):
                         if unidad[key][j][i] == 'X':
-                            self.__descansos_fila += 1
-                    if key != 'U5':
-                        if self.__descansos_fila != 4 and self.__descansos_fila != 5:
-                            print('ERROR en la cantidad de descansos por trabajador')
-                            print('Unidad: ' + key)
-                            print('El trabajador ' + str(j + 1) + ' tiene ' + str(self.__descansos_fila) + ' descanso(s).')
-                    else:
-                        if self.__descansos_fila != 22:
-                            print('ERROR en la cantidad de descansos por trabajador')
-                            print('Unidad: ' + key)
-                            print('El trabajador ' + str(j + 1) + ' tiene ' + str(self.__descansos_fila) + ' descanso(s).')
+                            self.descansos_trabajador_semana += 1
+                        if i == self.semana - 1:
+                            if key != 'reten':
+                                if self.descansos_trabajador_semana != 1:
+                                    print('ERROR en la cantidad de descansos por trabajador')
+                                    print('Unidad: ' + key)
+                                    print('El trabajador ' + str(j + 1) + ' tiene ' + str(self.descansos_trabajador_semana) + ' descanso(s).')
 
     # Validamos la cantidad de Ds y Ns en cada día - Validación por columna
     def validacion_turnos_columna(self):
         i = 0
         d_columna = n_columna = 0
-        for i in range(len(self.__rol_servicio[0]['U1'][0][0])):
-            for unidad in self.__rol_servicio:
+        for i in range(len(self.rol_servicio[0]['U1'][0])):
+            for unidad in self.rol_servicio:
                 for key, value in unidad.items():
                     for j in range(len(unidad[key])):
                         if unidad[key][j][i] == 'D':
                             d_columna += 1
                         elif unidad[key][j][i] == 'N':
                             n_columna += 1
-            if d_columna != 3 or n_columna != 5:
+            if d_columna != self.turno_dia or n_columna != self.turno_noche:
                 print('ERROR en la repartición de turnos por día.')
-                print('Columna [' + str(i) + ']:')
+                print('Columna [' + str(i + 1) + ']:')
                 print('D = ' + str(d_columna))
                 print('N = ' + str(n_columna))
             d_columna = n_columna = 0
@@ -77,9 +78,9 @@ class Main:
     #Validamos la distribución de Ds y Ns en cada unidad - Validación por turnos de unidad
     def validacion_turnos_unidad(self):
         i = 0
-        for unidad in self.__rol_servicio:
+        for unidad in self.rol_servicio:
             for key, value in unidad.items():
-                validacion = self.__unidad_validacion[key]
+                validacion = self.unidad_validacion[key]
                 for i in range(len(value[0])):
                     d = n = 0
                     for j in range(len(value)):
@@ -87,17 +88,16 @@ class Main:
                             d += 1
                         elif unidad[key][j][i] == 'N':
                             n += 1
-                    if key == 'U4':
-                        #Para el descansero aplicamos la lógica del algoritmo de la burbuja
-                        semana = 7
+                    if key == 'descansero':
+                        #Para el descansero aplicamos el intercambio de valores, como en el algoritmo de la burbúja
                         temp = 0
-                        if i == semana or i == semana * 2 or i == semana * 3 or i == semana * 4:
-                            temp = self.__unidad_validacion[key][0]
-                            self.__unidad_validacion[key][0] = self.__unidad_validacion[key][1]
-                            self.__unidad_validacion[key][1] = temp
-                    if d > self.__unidad_validacion[key][0]:
+                        if i == self.semana or i == self.semana * 2 or i == self.semana * 3 or i == self.semana * 4:
+                            temp = self.unidad_validacion[key][0]
+                            self.unidad_validacion[key][0] = self.unidad_validacion[key][1]
+                            self.unidad_validacion[key][1] = temp
+                    if d > self.unidad_validacion[key][0]:
                         print('ERROR, exceso de día(s) en la unidad: ' + key + '. ' + str(d) + ' días')
-                    elif n > self.__unidad_validacion[key][1]:
+                    elif n > self.unidad_validacion[key][1]:
                         print('ERROR, exceso de noche(s) en la unidad: ' + key + '. ' + str(n) + ' noches')
 
 
